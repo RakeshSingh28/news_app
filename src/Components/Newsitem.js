9c87a29d058218b140d50f1ee8d338e8eec55e25
Rakesh Singh <rakesh.singh@globalids.com>
2024-07-03 18:59:07 +0530

    component setup of newsapp

---
+++ src/Components/Newsitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        This is news item component.
+      </div>
+    );
+  }
+}
+
+export default Newsitem;
